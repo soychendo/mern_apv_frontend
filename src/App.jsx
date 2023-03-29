60337728ed25c5e5ade329e7adae8b75113626ec
chendo <chendoec@live.com>
2023-03-29 18:28:48 -0500

    Fixed Bug Confirm Email

--- src/App.jsx
+++ src/App.jsx
@@ -22,14 +22,14 @@ function App() {
    <AuthProvider>
     <PacientesProvider>
       <Routes>
-        <Route path='/' element={<AuthLayout />}>
+        <Route path='/#/' element={<AuthLayout />}>
           <Route index element={<Login />} />
           <Route path='registrar' element={<Registrar />} />
           <Route path='olvide-password' element={<OlvidePassword />} />
           <Route path='olvide-password/:token' element={<NuevoPassword />} />
           <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
         </Route>
-        <Route path='/admin' element={<RutaProtegida />}>
+        <Route path='/#/admin' element={<RutaProtegida />}>
           <Route index element={<AdministrarPacientes />} />
           <Route path='perfil' element={<EditarPerfil />} />
           <Route path='cambiar-password' element={<CambiarPassword />} />
