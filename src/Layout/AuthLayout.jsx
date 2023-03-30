89695f899a6beac02aec54c46908a3b0562bce68
chendo <chendoec@live.com>
2023-03-29 19:47:08 -0500

    Added validation on Public Routes Token

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -1,6 +1,23 @@
 import { Outlet } from "react-router-dom"
+import jwt_decode from 'jwt-decode';
+import { Navigate } from 'react-router-dom';
 
 export const AuthLayout = () => {
+
+  const token = localStorage.getItem('token');
+  if (token) {
+    const decodedToken = jwt_decode(token);
+    const currentTime = Date.now() / 1000; // Divide by 1000 to convert to seconds
+    if (decodedToken.exp < currentTime) {
+      // Token has expired, clear localStorage and redirect to login
+      localStorage.removeItem('token');
+      return <Navigate to="/" />;
+    } else {
+      // Token is still valid, redirect to admin area
+      return <Navigate to="/admin" />;
+    }
+  }
+
   return (
     <>
       <main 
